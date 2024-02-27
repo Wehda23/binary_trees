93aaf586ccd368df52e0d2f81cc81d6fb338041e
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-27 20:45:00 +0200

    Task 6 done

--- binary_trees.h
+++ binary_trees.h
@@ -47,4 +47,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 /* 5-binary_tree_is_root.c */
 int binary_tree_is_root(const binary_tree_t *node);
 
+/* 6-binary_tree_preorder.c */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /* End of binary_trees.h */
