fb36d70bec226522a7f1be5d1e2ec5eaf8c92700
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-27 16:32:22 +0200

    First task done

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - Function that creates and links new node to parent node
+ * @parent: Pointer to the parent node to link the new node to.
+ * @value: Value of the  new node.
+ * Return: Pointer to the new node created.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+    if (!new)
+    {
+        printf("Failed to malloc new node\n");
+        return (NULL);
+    }
+
+    new->n = value;
+    new->parent = parent;
+
+    return (new);
+}
