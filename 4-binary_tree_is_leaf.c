8cb1eefcac277656de0304ca48b4e7bfbce55e11
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-27 19:49:38 +0200

    Task 4 done

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ * @node: Pointer to a binary tree node
+ * Return: 1 in case if node is a leaf otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if ((node->left == NULL && node->right == NULL))
+    {
+        return (1);
+    }
+    return (0);
+}
