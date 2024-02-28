322b0d94652efc9f5b6b5fe1cf8ebf97eba5138a
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-28 14:09:33 +0200

    Adjusted task 9 to pass betty checks

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t count = 0, count_left = 0;
+	size_t count = 0, count_left = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    
-    count = tree->right? 1 + binary_tree_height(tree->right) : 0;
-    count_left = tree->left? 1 + binary_tree_height(tree->left) : 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    return ((count > count_left)? count : count_left);
-    
+	count = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	count_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+
+	return ((count > count_left) ? count : count_left);
+
 }
