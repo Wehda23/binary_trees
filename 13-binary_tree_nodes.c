3ba03b2aa722cd88e434697e80428a81582b22f5
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-28 17:27:06 +0200

    Binary Tree Nodes done

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,16 +9,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    if (tree == NULL)
-        return (0);
-    
-    if(tree->left || tree->right)
-        count++;
-    
-    count += binary_tree_nodes(tree->left);
-    count += binary_tree_nodes(tree->right);
+	if (tree == NULL)
+		return (0);
 
-    return (count);
+	if (tree->left || tree->right)
+		count++;
+
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
 }
