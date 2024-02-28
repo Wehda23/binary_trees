a409f618343876fb837182e961530149a88ecc73
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-28 17:49:13 +0200

    Task 14 adjusted to pass betty checker

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,15 +7,15 @@
  */
 size_t binary_tree_height_d(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height_d(tree->left);
-    right_height = binary_tree_height_d(tree->right);
+	left_height = binary_tree_height_d(tree->left);
+	right_height = binary_tree_height_d(tree->right);
 
-    return (1 + (left_height > right_height ? left_height : right_height));
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
 /**
@@ -25,13 +25,13 @@ size_t binary_tree_height_d(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
-    
-    left_height = binary_tree_height_d(tree->left);
-    right_height = binary_tree_height_d(tree->right);
+	if (tree == NULL)
+		return (0);
 
-    return (left_height - right_height);
+	left_height = binary_tree_height_d(tree->left);
+	right_height = binary_tree_height_d(tree->right);
+
+	return (left_height - right_height);
 }
