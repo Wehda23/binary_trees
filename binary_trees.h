6e54e1976b1e6a54ad10fde8920730f9f0c2431e
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-28 17:25:30 +0200

    Task 13 Nodes

--- binary_trees.h
+++ binary_trees.h
@@ -68,4 +68,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 /* 12-binary_tree_leaves.c */
 size_t binary_tree_leaves(const binary_tree_t *tree);
 
+/* 13-binary_tree_nodes.c */
+
+
 #endif /* End of binary_trees.h */
