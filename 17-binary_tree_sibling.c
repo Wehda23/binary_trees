d2988ea86f6df682f4ea6d1a375ef5e6174355f7
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-29 18:27:30 +0200

    Test in Linux enviroment

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_sibling - that finds the sibling of a node
+ * @tree: Node in a binary tree
+ * Return: pointer to the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    binary_tree_t *temp = node->parent;
+
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
+    
+    if (temp->left->n == node->n)
+        return (temp->right);
+    else
+        return (temp->left);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -80,4 +80,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 /* 16-binary_tree_is_perfect.c */
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
+/* 17-binary_tree_sibling.c */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 #endif /* End of binary_trees.h */
