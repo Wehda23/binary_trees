e923ea45a4962cb6dcf85261da841b864d10d2c0
Waheed Khaled <waheedkhaled95@gmail.com>
2024-02-29 18:28:53 +0200

    Task 17 Ready for Checker

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,18 +3,18 @@
 
 /**
  * binary_tree_sibling - that finds the sibling of a node
- * @tree: Node in a binary tree
+ * @node: Node in a binary tree
  * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    binary_tree_t *temp = node->parent;
+	binary_tree_t *temp = node->parent;
 
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
-    
-    if (temp->left->n == node->n)
-        return (temp->right);
-    else
-        return (temp->left);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (temp->left->n == node->n)
+		return (temp->right);
+	else
+		return (temp->left);
 }
