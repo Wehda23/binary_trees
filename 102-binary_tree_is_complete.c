8e5a56b9a9a21d8ca1a9e2cd2c80ebb5ebbd74ed
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-01 04:31:36 +0200

    Test

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -9,16 +9,16 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    int right = 0, left = 0;
+	int right = 0, left = 0;
 
-    if (tree == NULL)
-        return (0);
-    
-    if (tree->left == NULL  && tree->right == NULL)
-        return (1);
-    
-    right  = binary_tree_is_complete(tree->right);
-    left   = binary_tree_is_complete(tree->left);
+	if (tree == NULL)
+		return (0);
 
-    return (right && left);
+	if (tree->left == NULL  && tree->right == NULL)
+		return (1);
+
+	right  = binary_tree_is_complete(tree->right);
+	left   = binary_tree_is_complete(tree->left);
+
+	return (right && left);
 }
