066100b715c7fddb9922e4761d6d2c07511cb8e9
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-01 04:16:42 +0200

    Task 101 Test

--- binary_trees.h
+++ binary_trees.h
@@ -89,6 +89,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 /* 100-binary_trees_ancestor.c */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
+/* 101-binary_tree_levelorder.c */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
 /* 103-binary_tree_roatate_left.c */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
