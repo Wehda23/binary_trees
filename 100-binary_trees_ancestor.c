ddcd9c2e4ba16d3034a3691529c9b3a136e05041
Waheed Khaled <waheedkhaled95@gmail.com>
2024-03-01 03:10:55 +0200

    Task 19 Ready for Tests

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,7 +9,7 @@
  * Return: Pointer to the common node or NULL
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                    const binary_tree_t *second)
+		const binary_tree_t *second)
 {
 	binary_tree_t *first_parent, *second_parent;
 
@@ -24,13 +24,13 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	first_parent = first->parent;
 	second_parent = second->parent;
-	if (first_parent == NULL || first == second_parent || 
-    (!first_parent->parent && second_parent))
+	if (first_parent == NULL || first == second_parent ||
+			(!first_parent->parent && second_parent))
 	{
 		return (binary_trees_ancestor(first, second_parent));
 	}
-	else if (second_parent == NULL || first_parent == second || 
-    (!second_parent->parent && first_parent))
+	else if (second_parent == NULL || first_parent == second ||
+			(!second_parent->parent && first_parent))
 	{
 		return (binary_trees_ancestor(first_parent, second));
 	}
